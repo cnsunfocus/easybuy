28d65ddd5895ee73a96d882d8ceebb079512185b
cnsunfocus <reuental@126.com>
2018-01-12 17:54:51 +0800

    delete

--- bak/config/dev.env.js
+++
@@ -1,7 +0,0 @@
-var merge = require('webpack-merge')
-var prodEnv = require('./prod.env')
-var express = require('express')
-var proxyMiddleware = require('http-proxy-middleware')
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
-})
